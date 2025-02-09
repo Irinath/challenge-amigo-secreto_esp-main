c5b9d036a94cb17f1fb7127ee7cf5b675acbfeeb
Valeria Thomas <thomas.valeria@gmail.com>
2025-02-09 18:48:25 -0300

    modificaciones al trabajo

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica
 //  para resolver el problema.
 
-let nombreAmigo = [];
+let listaNombresAmigo = [];
+let nombreAmigo = '';
 
-function amigoSecreto(){
+function agregarAmigos(){
+    //Leer nombre del placeholder
+    let nombreAmigo = document.getElementById('amigo');
+    //click on botón "Añadir"
+    HTMLButtonElement.onclick()
+    listaNombresAmigo.push(nombreAmigo);
+    //agrega nombre a la lista "nombreAmigo"
 
-    
 }
+
+function sortearAmigos(){
+    //click en botón "sortearAmigo"
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = ''; 
+}
+
+//Sobre el campo "Escribe un nombre" se va a añadir el nombre al array nombreAmigo y debe accionarse con el botón añadir
+//através de la función "agregarAmigos"
