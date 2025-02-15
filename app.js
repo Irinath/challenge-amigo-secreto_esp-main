5122a20d2fe2c18fdd7433c7c2b80c271985a60b
Valeria Thomas <thomas.valeria@gmail.com>
2025-02-14 21:02:22 -0300

    Se resolvió la función agregar amgo y el método de validación. Se incorporó una ayuda de trello

--- app.js
+++ app.js
@@ -7,28 +7,53 @@ let nombreAmigo = '';
 
 
 function agregarAmigo(){
-    alert('Esto es un click');
     //Leer nombre del placeholder
-    let nombreAmigo = document.getElementById('amigo');
+    let nombreAmigo = document.getElementById('amigo').value.trim();
     //click on botón "Añadir"
-    HTMLButtonElement.onclick()
+    //listaNombresAmigo.push(nombreAmigo);
+    console.log(listaNombresAmigo); //Eliminar
+    console.log(contarCaracteres(nombreAmigo)); //Eliminar
+    //limpiarCaja();
+    //Verificar que el campo no esté vacío
+    if (contarCaracteres(nombreAmigo) >= 2){
+        //agrega nombre a la lista "nombreAmigo"
     listaNombresAmigo.push(nombreAmigo);
-    //agrega nombre a la lista "nombreAmigo"
-
+    console.log(listaNombresAmigo);
+    } else {       
+        //alert('El nombre debe contener al menos 2 (dos) letras');
+        cambiarElemento('h2', 'El nombre debe contener al menos 2 (dos) letras');
+        limpiarCaja();
+        return;
+    }
+    limpiarCaja();
+    cambiarElemento('h2', 'Escribe el nombre de tu amigo');
 }
 
 function sortearAmigos(){
     //click en botón "sortearAmigo"
 }
 
+function cargarLista(){
+    //Lee el nombre de la lista 
+    //y lo agrega en el HTML en el elemneto "ul"
+}
+
 function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = ''; 
+    document.querySelector('#amigo').value = ''; 
 }
 
 function cambiarElemento(elemento, texto) {
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
+    return;
 }
+
+function contarCaracteres(nombreAmigo) {
+    //Cuenta los caracteres del nombre del amigo ingresado
+    let caracteres = nombreAmigo.length;
+    //retorna el número
+    return (caracteres);
+} 
 
 cambiarElemento('h2', 'Escriba el nombre de su Amigo');
 //Sobre el campo "Escribe un nombre" se va a añadir el nombre al array nombreAmigo y debe accionarse con el botón añadir
---
+++ ayudaTrello.md
@@ -0,0 +1,11 @@
+Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
+
+Tareas específicas:
+
+1_Obtener el elemento de la lista: Utilizar document.getElementById() o document.querySelector() para seleccionar la lista donde se mostrarán los amigos.
+
+2_Limpiar la lista existente: Establecer lista.innerHTML = "" para asegurarse de que no haya duplicados al actualizar.
+
+3_Iterar sobre el arreglo: Usa un bucle for para recorrer el arreglo amigos y crear elementos de lista (<li>) para cada título.
+
+4_Agregar elementos a la lista: Para cada amigo, crear un nuevo elemento de lista.
